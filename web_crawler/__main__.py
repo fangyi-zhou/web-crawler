6b29b13505a6ee716b1721d5a840cc2bf2a3b9d9
Fangyi Zhou <me@fangyi.io>
2025-02-27 22:52:33 +0000

    Implement crawling

--- web_crawler/__main__.py
+++ web_crawler/__main__.py
@@ -2,6 +2,7 @@ import asyncio
 
 from aiohttp import ClientSession
 
+from web_crawler.crawler import Crawler
 from web_crawler.fetcher.aiohttp_fetcher import AiohttpFetcher
 from web_crawler.link_processor import find_links
 
@@ -9,10 +10,9 @@ from web_crawler.link_processor import find_links
 async def test():
     session = ClientSession()
     fetcher = AiohttpFetcher(session=session)
+    crawler = Crawler(fetcher=fetcher)
     url = "https://example.com/"
-    content = await fetcher.get_content(url)
-    links = find_links(content, base_url=url)
-    print(links)
+    await crawler.start(url)
 
 
 asyncio.run(test())
---
+++ web_crawler/crawler.py
@@ -0,0 +1,32 @@
+from loguru import logger
+
+from web_crawler.fetcher.common import Fetcher
+from web_crawler.link_processor import find_links
+
+
+class Crawler:
+    fetcher: Fetcher
+    visited: set[str]
+
+    def __init__(self, fetcher: Fetcher):
+        self.fetcher = fetcher
+        self.visited = set()
+
+    async def start(self, start_url: str) -> None:
+        worklist: list[str] = [start_url]
+        while worklist:
+            url = worklist.pop()
+            if url in self.visited:
+                continue
+            self.visited.add(url)
+            content = await self.fetcher.get_content(url)
+            if content is None:
+                continue
+            links: set[str] = set(
+                filter(
+                    lambda link: link.startswith(start_url),
+                    find_links(content, base_url=url),
+                )
+            )
+            logger.info("URL {} has following links {}", url, links)
+            worklist.extend(links - self.visited)
