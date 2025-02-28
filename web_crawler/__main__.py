d2794c620681e8ad441dfadfb9536a02496138e4
Fangyi Zhou <me@fangyi.io>
2025-02-28 15:53:08 +0000

    Implement a simple argument parser

--- web_crawler/__main__.py
+++ web_crawler/__main__.py
@@ -1,14 +1,21 @@
+import argparse
 import asyncio
 
 from web_crawler.crawler import Crawler
 from web_crawler.fetcher.aiohttp_fetcher import AiohttpFetcher
 
 
-async def test():
+async def run_crawler(start_url: str):
     fetcher = AiohttpFetcher()
     crawler = Crawler(fetcher=fetcher)
-    url = "https://example.com/"
-    await crawler.start(url)
+    await crawler.start(start_url)
 
 
-asyncio.run(test())
+def main():
+    parser = argparse.ArgumentParser(description="A simple web crawler")
+    parser.add_argument("url", help="The URL to start the crawl from")
+    args = parser.parse_args()
+    asyncio.run(run_crawler(args.url))
+
+
+main()
