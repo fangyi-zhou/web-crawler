a77c01975985421aa861dedd2e2614699bc0b5e5
Fangyi Zhou <me@fangyi.io>
2025-03-03 21:09:45 +0000

    Remove some constants in aiohttp_fetcher

--- web_crawler/fetcher/aiohttp_fetcher.py
+++ web_crawler/fetcher/aiohttp_fetcher.py
@@ -7,17 +7,23 @@ from tenacity import retry, stop_after_attempt, wait_exponential_jitter
 CONTENT_TYPE_HTML = "text/html"
 STATUS_OK = 200
 
+DEFAULT_MAX_WAIT_TIME_IN_SECONDS = 30
+DEFAULT_MAX_RETRY_ATTEMPTS = 5
+
 
 class AiohttpFetcher:
     """A crawler implementation that uses aiohttp for asynchronous crawling"""
 
-    @retry(wait=wait_exponential_jitter(max=30), stop=stop_after_attempt(5))
+    @retry(
+        wait=wait_exponential_jitter(max=DEFAULT_MAX_WAIT_TIME_IN_SECONDS),
+        stop=stop_after_attempt(DEFAULT_MAX_RETRY_ATTEMPTS),
+    )
     async def get_content(self, url: str) -> Optional[str]:
         async with ClientSession() as session:
             async with session.get(
                 url, headers={"Accept": CONTENT_TYPE_HTML}
             ) as response:
-                if response.status != 200:
+                if response.status != STATUS_OK:
                     logger.warning(
                         "Got non-OK status {}, skipping. url = {}", response.status, url
                     )
